0d132b51293b592b1dbbb6744387242361e9a549
Vardan Makhsudyan <vardan17071992@mail.ru>
2020-05-26 13:53:15 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by MacBook Air on 26.05.2020.
+//  Copyright © 2020 VardanMakhsudyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
